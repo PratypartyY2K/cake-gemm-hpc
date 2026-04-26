2737187c965fb386e331330feefcfaca205a1e98
Pratyush Kumar <kumarkinshuk14@gmail.com>
2026-04-26 12:51:36 -0400

    Add initial CPU and GPU build scaffolding

---
+++ src/gpu/cublas_gemm.cu
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "CAKE GEMM GPU baseline\n";
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "CAKE GEMM CPU baseline\n";
+    return 0;
+}
